63b5dc8db5bc049ff45832f2ff4ab56de435ad87
Properti-X <th.kiene@gmail.com>
2025-12-22 15:37:06 +0100

    Remove sessionStorage export that causes error

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -8,10 +8,10 @@ import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prism
 import prisma from "./db.server";
 
 const shopify = shopifyApp({
-  apiKey: process.env.SHOPIFY_API_KEY || "",
+  apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
   apiVersion: ApiVersion.October25,
-  scopes: process.env.SCOPES?.split(",") || [],
+  scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
